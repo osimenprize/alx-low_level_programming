18d70c589eb258dd46d07a6e3ebf60d3221a94d2
osimenprize <prizeosimen@yahoo.com>
2023-08-18 19:03:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
